9f021687b0874f509dbb82053f6fd1ed15382283
Donika Juthani <39000402+Donika1999@users.noreply.github.com>
2019-10-12 16:55:20 +0530

    Create Best_time_to_buy_sell_stocks.cpp

---
+++ DP/Best_time_to_buy_sell_stocks.cpp
@@ -0,0 +1,16 @@
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def maxProfit(self, A):
+        if not A: return 0
+        xmin = A[0]
+        p = [0] * len(A)
+        for i, x in enumerate(A):
+            xmin = min(xmin, x)
+            p[i] = max(p[i-1], x - xmin)
+        profit = 0
+        xmax = A[len(A)-1]
+        for i in range(len(A)-1,-1,-1):
+            xmax = max(xmax, A[i])
+            profit = max(profit, p[i] + xmax - A[i])
+        return profit
